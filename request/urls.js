edce3fee2e16e8f969e77b6d82e10480152b2a37
Raúl Coral <rfcoralj@unal.edu.co>
2019-02-14 07:31:08 -0500

    Fix env variables names

--- request/urls.js
+++ request/urls.js
@@ -1,15 +1,15 @@
 // Recipes_ms
-const recipes_port = process.env.RECIPES_PORT || 8082
+const recipes_port = process.env.RECIPES_PORT || 8081
 const recipes_baseUrl = process.env.RECIPES_URL || '192.168.99.100'
 const recipes_url = `http://${recipes_baseUrl}:${recipes_port}`
 
 // Collections_ms
-const collections_port = process.env.COLLECTIONS_PORT || 8083
+const collections_port = process.env.COLLECTIONS_PORT || 8082
 const collections_baseUrl = process.env.COLLECTIONS_URL || '192.168.99.100'
 const collections_url = `http://${collections_baseUrl}:${collections_port}`
 
 // Users_ms
-const users_port = process.env.USERS_PORT || 8084
+const users_port = process.env.USERS_PORT || 8083
 const users_baseUrl = process.env.USERS_URL || '192.168.99.100'
 const users_url = `http://${users_baseUrl}:${users_port}`
 
@@ -19,8 +19,8 @@ const comments_baseUrl = process.env.COMMENTS_URL || '192.168.99.100'
 const comments_url = `http://${comments_baseUrl}:${comments_port}`
 
 // Join_ms
-const join_port = process.env.NOTIFICATIONS_PORT || 8086
-const join_baseUrl = process.env.NOTIFICATIONS_URL || '192.168.99.100'
+const join_port = process.env.JOIN_PORT || 8086
+const join_baseUrl = process.env.JOIN_URL || '192.168.99.100'
 const join_url = `http://${join_baseUrl}:${join_port}`
 
 module.exports = {
